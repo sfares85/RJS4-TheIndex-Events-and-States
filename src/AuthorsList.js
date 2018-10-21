85d36cb0c0633df09e4123f23aab261d45f084a1
Lailz <lailaabdulraheem@gmail.com>
2018-10-21 15:09:35 +0300

    Updated

--- src/AuthorsList.js
+++ src/AuthorsList.js
@@ -5,7 +5,11 @@ import AuthorCard from "./AuthorCard";
 
 function AuthorsList(props) {
   const authorCards = props.authors.map(author => (
-    <AuthorCard key={author.first_name + author.last_name} author={author} />
+    <AuthorCard
+      key={author.first_name + author.last_name}
+      author={author}
+      selectAuthor={props.selectAuthor}
+    />
   ));
 
   return (
